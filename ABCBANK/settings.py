670ea5788d056c3c2256472928a792405b3fe05e
nixen2802 <n.mandliya@somaiya.edu>
2021-08-19 23:47:25 +0530

    Updated settings

--- ABCBANK/settings.py
+++ ABCBANK/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tto&)2^b=()w*1$f^t!5o=s4eu5$p0*2=l@5ztudx60zw#4j&%'
-
+# SECRET_KEY = 'tto&)2^b=()w*1$f^t!5o=s4eu5$p0*2=l@5ztudx60zw#4j&%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

d7e3d055b751815d9ebc6a6d42b8cde7760e230b
nixen2802 <n.mandliya@somaiya.edu>
2021-08-20 23:15:30 +0530

    Minor changes

--- bank/views.py
+++ bank/views.py
@@ -94,9 +94,9 @@ def transfer_detail(request, pk):
                     trans_insert.amount=request.POST.get('amount')
                     trans_insert.status="Success"
                     trans_insert.date_of_transfer= datetime.datetime.now().strftime("%Y-%m-%d   %H:%M:%S")
-                    deduction=Customer.objects.get(cust_id=request.POST.get('sender'))
+                    deduction=Customer.objects.get(cust_id=int(request.POST.get('sender')))
                     deduction.balance=deduction.balance-float(request.POST.get('amount'))
-                    credetion=Customer.objects.get(cust_id=request.POST.get('receiver'))
+                    credetion=Customer.objects.get(cust_id=int(request.POST.get('receiver')))
                     credetion.balance=credetion.balance+float(request.POST.get('amount'))
                     deduction.save()
                     credetion.save()
